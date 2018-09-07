e592efc8294ba8e7ef1385be8025b4980773547b
wangliquan <liquan.wang@croninfo.com>
2018-09-07 14:55:23 +0800

    Initial commit

---
+++ AlamofireEncapsulation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlamofireEncapsulation
+//
+//  Created by Ethan.Wang on 2018/9/7.
+//  Copyright © 2018年 Ethan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform:ios, '9.0'
+target 'AlamofireEncapsulation' do
+use_frameworks!
+pod 'Alamofire'
+end
+
